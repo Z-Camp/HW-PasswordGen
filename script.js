60e538e3720cfe108e6a11b820b4eb044b0f15b5
Romo <60673021+Z-Camp@users.noreply.github.com>
2020-02-29 02:09:39 -0500

    added copy button

--- script.js
+++ script.js
@@ -2,27 +2,23 @@ var lowercaseString = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericString = "1234657890";
 var symbolString = "!@#$%^&*()<>?/.,;[]-=_+|";
-
-// var lowercaseArray = lowercaseString.split("")
-// var uppercaseArray = uppercaseString.split("")
-// var numericArray = numericString.split("")
-// var symbolArray= symbolString.split("")
-
-// console.log(lowercaseArray)
-// console.log(uppercaseArray)
-// console.log(numericArray)
-// console.log(symbolArray)
-
 var button = document.getElementById("button");
+var copyButton = document.getElementById("copyButton");
 
 function generatePassword() {
-  var input = prompt(
+  var userInput = prompt(
     "How long will the password be? (must be between 8 and 128"
   );
-  input = parseInt(input);
-  if (input < 8 || input > 128 || input == null || typeof(input) != Number) {
+  userInput = parseInt(userInput);
+  console.log(userInput);
+  if (
+    userInput < 8 ||
+    userInput > 128 ||
+    userInput == null
+    //  || typeof(userInput) != Number
+  ) {
     alert("please select a length between 8 and 128");
-    generatePassword()
+    generatePassword();
   } else {
     var values = "";
     // "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
@@ -58,12 +54,18 @@ function generatePassword() {
     }
 
     var password = "";
-    for (var i = 0; i < input; i++) {
-      password += values.charAt(Math.floor(Math.random() * values.length));
+    for (var i = 0; i < userInput; i++) {
+      password += values.charAt(Math.floor(Math.random() * values.length - 1));
     }
     console.log(password);
-    document.querySelector("#display").setAttribute("placeholder", password);
+    document.querySelector("#display").value = password
   }
+}
+function copyPassword() {
+  document.querySelector("#display").select();
+  document.execCommand("Copy");
+  alert("copied password to clipboard");
 }
 
 button.addEventListener("click", generatePassword);
+copyButton.addEventListener("click", copyPassword);
